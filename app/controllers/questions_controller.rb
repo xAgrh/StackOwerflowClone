9f0c92cfcdf401aa22a8bc2984059da2a7d9637f
xAgrh <obsnrd@gmail.com>
2014-05-08 16:57:34 +0400

    Закончил тесты экшенов контроллера Question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :load_question, only: [:show, :edit]
+  before_action :load_question, only: [:show, :edit, :update, :destroy]
   
   def index
     @questions = Question.all
@@ -15,10 +15,35 @@ class QuestionsController < ApplicationController
   def edit
   end
   
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+  
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @question.destroy
+    redirect_to questions_path
+  end
+  
   private
   
   def load_question
     @question = Question.find(params[:id])
   end
   
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
 end
