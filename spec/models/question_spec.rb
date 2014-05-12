f699fe7becd339a64520b55a312a1a65e28e7718
xAgrh <obsnrd@gmail.com>
2014-05-12 18:35:43 +0400

    Add association between question and answers

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe Question do
-  
+  it { should have_many :answers }
+    
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
 
