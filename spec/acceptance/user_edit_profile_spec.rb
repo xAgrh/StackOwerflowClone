e825cdef456e969ec06c8b1f63014874c8e0e914
xAgrh <obsnrd@gmail.com>
2014-06-17 17:40:44 +0400

    Profile test

---
+++ spec/acceptance/user_edit_profile_spec.rb
@@ -0,0 +1,31 @@
+require_relative 'acceptance_helper'
+
+feature 'User edit his profile', %q(
+  In order to correct information about me
+  As an authenticated User
+  I want to be able edit my profile data
+  ) do
+    given!(:user) { create :user }
+
+    scenario 'Guest not see link to edit profile' do
+      visit questions_path
+
+      expect(page).to_not have_link 'Profile', href: edit_user_registration_path
+    end
+
+  context 'User sign in' do
+    before { sign_in user }
+
+    scenario 'edit profile data' do
+      visit questions_path
+      click_on 'Profile'
+      fill_in 'Nickname', with: 'Ololo'
+      fill_in 'Current password', with: user.password
+      click_on 'Сохранить User'
+      click_on 'Profile'
+      
+      expect(page).to have_content 'Ololo'
+    end
+
+  end
+end
